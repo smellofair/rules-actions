9035737d787f3357adcbd4ac06fb31e5e44a9351
Julian Naydichev <git+plutonium@j.diode.pw>
2024-10-18 13:03:07 +0200

    add more logging

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 
 try {
   const hubRepo = core.getInput("hub-repo");
   const payload = github.context.payload;
   core.info(JSON.stringify(payload, null, 2));
-  core.summary.addDetails("Payload", JSON.stringify(payload, null, 2));
-  core.summary.addDetails("Hub Repo", hubRepo);
+  core.info("Hub Repo: " + hubRepo);
+
+  core.info("Files: " + fs.readdirSync("./").join("\n"));
+
 } catch (error) {
   core.setFailed(error.message);
 }
